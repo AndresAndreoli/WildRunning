a42f3a913bcbc1af3d43921d1ff2f31ddec03940
Andres Andreoli <andresandreolii@gmail.com>
2022-05-30 07:13:40 -0300

    Fun login created

--- app/src/main/java/com/example/wildrunning/LoginActivity.kt
+++ app/src/main/java/com/example/wildrunning/LoginActivity.kt
@@ -1,11 +1,72 @@
 package com.example.wildrunning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.LinearLayout
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object{ //Creo esto para poderlo utilizar en el resto de activities
+        lateinit var userEmail: String
+        lateinit var providerSession: String
+    }
+
+    private var email by Delegates.notNull<String>() // Es para inidicar que no va a ser nulo
+    private var password by Delegates.notNull<String>()
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var lyTerms: LinearLayout //layout de terminis y condiceciones, para ocultarlo
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        lyTerms = findViewById(R.id.lyTerms)
+        lyTerms.visibility = View.INVISIBLE // Ocultar LAYOUT
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    fun login(view: View){
+        loginUser() // al ser "login" una fun publica, creo esta fun privada para mantener la privacidad
+    }
+
+    private fun loginUser(){
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){task -> //esta variable tiene la informacion de si el inicio de sesion fue con exito o no
+                if (task.isSuccessful) goHome(email, "email")
+                else{
+                    if (lyTerms.visibility == View.INVISIBLE) lyTerms.visibility = View.VISIBLE
+                    else {
+                        var cbAcept = findViewById<CheckBox>(R.id.cbAccept)
+                        if (cbAcept.isChecked) register()
+                    }
+                }
+            }
+    }
+
+    private fun goHome(email: String, provider: String){
+        userEmail = email
+        providerSession = provider //para saber como se inicio sesion
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun register(){
+
     }
 }
