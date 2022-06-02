c9ee2439f838677e13a758a132ca41b0acd21fab
Andres Andreoli <andresandreolii@gmail.com>
2022-06-02 09:24:37 -0300

    SignIn with Google added

--- app/src/main/java/com/example/wildrunning/LoginActivity.kt
+++ app/src/main/java/com/example/wildrunning/LoginActivity.kt
@@ -1,18 +1,24 @@
 package com.example.wildrunning
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
-import com.google.firebase.FirebaseApp
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.properties.Delegates
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -28,6 +34,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var lyTerms: LinearLayout //layout de terminis y condiceciones, para ocultarlo
 
     private lateinit var mAuth: FirebaseAuth
+    private var RESULT_CODE_GOOGLE_SIGN_IN = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -140,6 +147,46 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun signInGoogle(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
+        var mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleSignInClient.signOut()
+
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RESULT_CODE_GOOGLE_SIGN_IN)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RESULT_CODE_GOOGLE_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
+                if (account!=null){
+                    email = account.email!!
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    // SignIn
+                    mAuth.signInWithCredential(credential)
+                        .addOnCompleteListener{ task ->
+                            if (task.isSuccessful)
+                                goHome(email, "Google")
+                            else
+                                Toast.makeText(this, "Error en la conexion con Google", Toast.LENGTH_SHORT).show()
+                        }
+                }
+
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Error en la conexion con Google", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
+
 }
